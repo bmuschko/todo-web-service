3cbed651478e2337a6c628d705c5ad96470aae99
Benjamin Muschko <benjamin.muschko@gmail.com>
2018-10-04 13:17:32 -0600

    Logger message if Docker secret file could be read

--- src/main/java/com/bmuschko/todo/webservice/env/DockerSecretsDatabasePasswordProcessor.java
+++ src/main/java/com/bmuschko/todo/webservice/env/DockerSecretsDatabasePasswordProcessor.java
@@ -1,5 +1,7 @@
 package com.bmuschko.todo.webservice.env;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
@@ -14,12 +16,18 @@ import java.util.Properties;
 
 public class DockerSecretsDatabasePasswordProcessor implements EnvironmentPostProcessor {
 
+    private final Logger logger = LoggerFactory.getLogger(DockerSecretsDatabasePasswordProcessor.class);
+
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
         Resource resource = new FileSystemResource("/run/secrets/db-password");
 
         if (resource.exists()) {
             try {
+                if (logger.isInfoEnabled()) {
+                    logger.info("Using database password from injected Docker secret file");
+                }
+
                 String dbPassword = StreamUtils.copyToString(resource.getInputStream(), Charset.defaultCharset());
                 Properties props = new Properties();
                 props.put("spring.datasource.password", dbPassword);
