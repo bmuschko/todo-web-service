b3f6d169ef670658ebc7fd43ff8969144a14af2e
Benjamin Muschko <benjamin.muschko@gmail.com>
2019-08-26 16:15:45 -0600

    Use Google Jib and Testcontainers

--- src/functionalTest/java/com/bmuschko/todo/webservice/ToDoWebServiceFunctionalTest.java
+++ src/functionalTest/java/com/bmuschko/todo/webservice/ToDoWebServiceFunctionalTest.java
@@ -1,22 +1,54 @@
 package com.bmuschko.todo.webservice;
 
 import com.bmuschko.todo.webservice.model.ToDoItem;
-import okhttp3.*;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Testcontainers
 public class ToDoWebServiceFunctionalTest {
 
-    private final static String WEB_SERVICE_URL = "http://localhost:8080";
+    private final static File DISTRIBUTION_DIR = new File(System.getProperty("distribution.dir"));
+    private final static String ARCHIVE_NAME = System.getProperty("archive.name");
     private final static String CONTEXT = "/todos";
     private final static MediaType JSON_MEDIA_TYPE = MediaType.get("application/json");
     private final OkHttpClient client = new OkHttpClient();
+
+    @Container
+    private GenericContainer appContainer = createContainer();
+
+    private static GenericContainer createContainer() {
+        return new GenericContainer(buildImageDockerfile())
+                .withExposedPorts(8080)
+                .waitingFor(Wait.forHttp("/actuator/health")
+                        .forStatusCode(200));
+    }
+
+    private static ImageFromDockerfile buildImageDockerfile() {
+        return new ImageFromDockerfile()
+                .withFileFromFile(ARCHIVE_NAME, new File(DISTRIBUTION_DIR, ARCHIVE_NAME))
+                .withDockerfileFromBuilder(builder -> builder
+                        .from("openjdk:jre-alpine")
+                        .copy(ARCHIVE_NAME, "/app/" + ARCHIVE_NAME)
+                        .entryPoint("java", "-jar", "/app/" + ARCHIVE_NAME)
+                        .build());
+    }
 
     @Test
     @DisplayName("can retrieve all items before and after inserting new ones")
@@ -56,7 +88,10 @@ public class ToDoWebServiceFunctionalTest {
 
     private URL buildEndpointUrl(String context) {
         StringBuilder url = new StringBuilder();
-        url.append(WEB_SERVICE_URL);
+        url.append("http://");
+        url.append(appContainer.getContainerIpAddress());
+        url.append(":");
+        url.append(appContainer.getFirstMappedPort());
         url.append(context);
 
         try {
